f71ce01f2ecef989be4a855294e8c1bda8ae3d1d
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-01-21 23:11:29 -0300

    Initial project structure

---
+++ src/main/java/com/fin_pulse/fin_pulse/FinPulseApplication.java
@@ -0,0 +1,13 @@
+package com.fin_pulse.fin_pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinPulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fin_pulse/fin_pulse/FinPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin_pulse.fin_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
